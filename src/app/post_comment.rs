c8691fa6eeb7288a6076d9c5f8f38cc8b533cad2
Ilya Bylich <ibylich@gmail.com>
2022-12-11 17:39:27 +0100

    implement comment creation endpoint

--- src/app/post_comment.rs
+++ src/app/post_comment.rs
@@ -1,16 +1,52 @@
-use crate::state::State;
+use crate::state::{Comment, State};
 use hyper::{
     header::{HeaderValue, CONTENT_TYPE},
-    Body, Request, Response,
+    Body, Request, Response, StatusCode,
 };
+use serde::Deserialize;
 use std::sync::{Arc, Mutex};
 
+#[derive(Deserialize, Debug)]
+struct FormData {
+    post_slug: String,
+    author: String,
+    body: String,
+}
+
 pub(crate) async fn post_comment(req: Request<Body>, state: Arc<Mutex<State>>) -> Response<Body> {
+    let body = match hyper::body::to_bytes(req.into_body()).await {
+        Ok(bytes) => bytes,
+        Err(e) => return internal_server_error(e),
+    };
+
+    let FormData {
+        post_slug,
+        author,
+        body,
+    } = match serde_json::from_slice::<FormData>(&body) {
+        Ok(data) => data,
+        Err(e) => return internal_server_error(e),
+    };
+
+    {
+        let mut state = state.lock().unwrap();
+        state.push(post_slug, Comment::new(author, body));
+    }
+
     let mut res = Response::new(Body::empty());
     res.headers_mut()
         .insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    *res.status_mut() = StatusCode::CREATED;
+    *res.body_mut() = Body::from("{}");
+    res
+}
 
-    let body = hyper::body::to_bytes(req.into_body()).await;
-
-    todo!()
+fn internal_server_error<E: std::fmt::Debug>(e: E) -> Response<Body> {
+    eprintln!("Internal Error: {:?}", e);
+    let mut res = Response::new(Body::empty());
+    res.headers_mut()
+        .insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    *res.body_mut() = Body::from("{}");
+    *res.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+    res
 }
