f0c6ca583736f91c300341031305bbba2a181567
Ilya Bylich <ibylich@gmail.com>
2022-12-11 19:40:52 +0100

    add embed action

--- src/app/embed.rs
+++ src/app/embed.rs
@@ -1,6 +1,20 @@
 use crate::state::State;
-use hyper::{Body, Request, Response};
+use hyper::{
+    header::{HeaderValue, CONTENT_TYPE},
+    Body, Request, Response,
+};
 
 pub(crate) fn embed(_req: Request<Body>, _state: State) -> Response<Body> {
-    todo!()
+    let mut res = if cfg!(debug_assertions) {
+        Response::new(Body::from(std::fs::read("embed.html").unwrap()))
+    } else {
+        Response::new(Body::from(
+            include_bytes!("../../embed.html") as &'static [u8]
+        ))
+    };
+
+    res.headers_mut()
+        .insert(CONTENT_TYPE, HeaderValue::from_static("text/html"));
+
+    res
 }
