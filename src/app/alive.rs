423db3e1a18f8a18714279acadd94a8f911ba410
Ilya Bylich <ibylich@gmail.com>
2023-07-07 21:40:09 +0200

    start from scratch

--- src/app/alive.rs
+++
@@ -1,9 +0,0 @@
-use crate::state::State;
-use hyper::{Body, Request, Response};
-
-pub(crate) fn alive(
-    _req: Request<Body>,
-    _state: State,
-) -> Result<Response<Body>, Box<dyn std::error::Error>> {
-    Ok(Response::new(Body::from("Alive")))
-}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
-use hyper::service::{make_service_fn, service_fn};
-use hyper::Server;
-use std::convert::Infallible;
-
-mod app;
-use app::app;
-
-mod state;
-use state::State;
-
-mod github;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:3000".parse()?;
-    let state = State::initial().await?;
-
-    println!("Booting initial state: {:#?}", state.debug().await);
-
-    let server = Server::bind(&addr).serve(make_service_fn(move |_conn| {
-        let state = state.clone();
-        async {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                let state = state.clone();
-                app(req, state)
-            }))
-        }
-    }));
-
-    println!("Starting server on http://127.0.0.1:3000");
-
-    if let Err(e) = server.await {
-        eprintln!("Failed to start server: {}", e);
-    }
-
     Ok(())
 }
