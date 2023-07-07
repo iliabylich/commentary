fbbc6df730420978664a4c790076cea4e06c9b82
Ilya Bylich <ibylich@gmail.com>
2023-07-07 21:47:59 +0200

    connect to the database

---
+++ src/database.rs
@@ -0,0 +1,24 @@
+use sqlx::sqlite::SqlitePool;
+
+use crate::config::Config;
+
+#[derive(Clone, Debug)]
+pub(crate) struct Database {
+    pub(crate) pool: SqlitePool,
+}
+
+fn database_url() -> String {
+    let config = Config::global();
+    format!("sqlite:{}", config.database_path)
+}
+
+impl Database {
+    pub(crate) async fn new() -> Self {
+        let pool = SqlitePool::connect(&database_url())
+            .await
+            .expect("Failed to connect to sqlite");
+        println!("Connected to sqlite");
+
+        Self { pool }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod config;
+mod database;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use crate::config::Config;
+    use crate::{config::Config, database::Database};
 
     Config::load();
+
+    let db = Database::new().await;
 
     Ok(())
 }
