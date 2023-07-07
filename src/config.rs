16c43b1be5b9c1a1c1944c67e68140d1f3d5cefe
Ilya Bylich <ibylich@gmail.com>
2023-07-07 21:46:38 +0200

    load the config

---
+++ src/config.rs
@@ -0,0 +1,29 @@
+use tokio::sync::OnceCell;
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct Config {
+    pub(crate) database_path: String,
+}
+
+static CONFIG: OnceCell<Config> = OnceCell::const_new();
+
+impl Config {
+    pub(crate) fn load() {
+        let path = if cfg!(debug_assertions) {
+            std::env::var("COMMENTARY_CONFIG_PATH")
+                .expect("No COMMENTARY_CONFIG_PATH environment variable set")
+        } else {
+            String::from("/etc/commentary.json")
+        };
+
+        let config_file = std::fs::read_to_string(&path).expect("Failed to read config file");
+        let config: Config =
+            serde_json::from_str(&config_file).expect("Failed to parse config file");
+        CONFIG.set(config).expect("Config has already been loaded");
+    }
+
+    pub(crate) fn global() -> &'static Config {
+        CONFIG.get().expect("Config is not loaded")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+mod config;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::config::Config;
+
+    Config::load();
+
     Ok(())
 }
